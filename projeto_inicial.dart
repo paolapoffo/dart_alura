a774f5cfbfbd5c9f992baf1f597cce85d4d78c16
Paola Poffo <paolapoffo1@gmail.com>
2022-07-04 19:09:04 -0300

    Dart: criando e manipulando variáveis e listas

--- projeto_inicial.dart
+++ projeto_inicial.dart
@@ -6,6 +6,8 @@ void main() {
   const String nome = "Paola"; //utilizando const (não altera)
   final String apelido = "Lola"; //utilizando final (não altera, não precisa ser definido de imediato)
   //utilizar var quando não se tem certeza do tipo de informação que ela vai retornar
+  bool maiorDeIdade;
+  int energia = 100;
 
   print(idade);
   print(altura);
@@ -13,12 +15,32 @@ void main() {
 
   print (nome + apelido); //concatenação
 
+  if (idade>=18){
+    maiorDeIdade = true;
+  } else {
+    maiorDeIdade = false;
+  }
+
+  for (int i=0; i<5; i++) {
+    print("Concluí $i voltas");
+  }
+
+  while(energia>0){
+  print("Mais uma repetição");
+  energia = energia - 10;
+  }
+
+  do{
+    print("Mais uma repetição");
+    energia = energia - 10;
+  }while (energia>0);
+
   List<dynamic> paola = [idade,altura,geek,nome,apelido]; //lista dinâmica, utilizando os objetos
   print(paola[2]); //imprimir apenas o que foi pre definido a partir do numero
 
   print ("Eu sou ${paola[4]}, \n" //(\n quebrar linha)
       "mas meu nome completo é ${paola[3]}, \n" //${} = interpolação, referênciar um objeto complexo
-      "tenho ${paola[0] = 25} anos de idade, \n" //${} = interpolação, referênciar um objeto complexo aplicando como variável
+      "tenho ${paola[0] = 25} anos de idade,Eu sou maior de idade? $maiorDeIdade \n" //${} = interpolação, referênciar um objeto complexo aplicando como variável
       "eu me considero geek? $geek."); // $ = interpolação, referênciar um objeto
 
 
